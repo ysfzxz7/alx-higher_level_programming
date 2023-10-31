c9179bc081fc04bf659fe6f498c103f696c5f151
ysfzxz7 <bbbmaroc83@gmail.com>
2023-10-31 07:03:28 -0700

    second test

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,5 @@
 for i in range(100):
     if i == 99:
         print("{}".format(i))
-    elif("{}{}, ".format("0" if i < 10 else "",i), end='')
+    else:
+        print("{}{}, ".format("0" if i < 10 else "",i), end="")
