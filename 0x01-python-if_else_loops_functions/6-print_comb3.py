b8b4fa6be96dbb0b5278a09a07af5d9079ad0631
ysfzxz7 <bbbmaroc83@gmail.com>
2023-10-31 07:19:55 -0700

    second test

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
-for l in range(10):
-    for r in range(10):
-        if l < r:
-            print("{}{}".format(l, r), end="")
-            print("{}".format("\n" if l == 8 and r == 9 else ", "), end="")
-            
-
+for left in range(10):
+    for right in range(10):
+        if left < right:
+            print("{}{}".format(left, right), end="")
+            print("{}".format("\n" if left == 8 and right == 9 else ", "), end="")
