21345a29d20b910f1367dcbc33d104ebd2ec2e6a
ysfzxz7 <bbbmaroc83@gmail.com>
2023-10-31 08:17:49 -0700

    second test

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
     for i in str:
         n = ord(i)
         char = chr(n - 32) if n in range(97, 123) else i
-        print(char, end="")
+        print("{}".format(char), end="")
     print()
