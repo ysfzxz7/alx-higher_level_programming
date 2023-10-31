0629ce3a71a08b5a9fb45a925dd6b243b3587d37
ysfzxz7 <bbbmaroc83@gmail.com>
2023-10-31 12:27:06 -0700

    need to be complete

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+i = 90
+while i > 64:
+        if i % 2 == 0:
+                print("{}".format(chr(i)), end="")
+        else:
+                print("{}".format(chr(i + 32)),end="" )
+        i -= 1
