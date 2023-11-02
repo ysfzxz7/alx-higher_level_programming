276f00d42b4f050e19618230ff247ba441d0f318
ysfzxz7 <bbbmaroc83@gmail.com>
2023-11-02 07:41:44 -0700

    argc and argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from sys import argv
-    lenght = len(argv)
-    i = 1
+    from sys import argv as ar
 
-    if lenght == 1:
-        print("{} arguments.".format(lenght - 1))
-    if lenght > 1:
-        if lenght == 2:
-            print("{} argument:".format(lenght -1))
-            print("1: {}".format(argv[1]))
-        else:
-            print("{} arguments:".format(lenght))
-            while i < lenght:
-                print("{:d} : {}".format(i, argv[i]))
-                i += 1
+    length = len(ar)
+    status = ""
+    if length == 1:
+        status = "s."
+    elif length == 2:
+        status = ":"
+    else:
+        status = "s:"
+    print("{} argument{}".format(length - 1, status))
+    if length > 1:
+        for i in range(1, len(ar)):
+            print("{}: {}".format(i, ar[i]))
