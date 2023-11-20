899bf77563c0d3aae441dfe67e47e5fe68380113
ysfzxz7 <bbbmaroc83@gmail.com>
2023-11-20 13:20:01 +0100

    $

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             printed = printed + 1
         except:
-            continue
+            print("")
+            return printed
     print("")
     return printed
