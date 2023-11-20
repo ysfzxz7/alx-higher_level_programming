0a0376792d69c98bc4f729744c9cc516bb600f2f
ysfzxz7 <bbbmaroc83@gmail.com>
2023-11-20 13:52:18 +0100

    the second task

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,6 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             printed = printed + 1
         except Exception as e:
-            pass
+            continue
     print("")
     return (printed)
