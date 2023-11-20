de82a099671db18efdefda99a33544bf19fe2179
ysfzxz7 <bbbmaroc83@gmail.com>
2023-11-20 16:30:47 +0100

    push me 101

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+import sys
 
 def safe_function(fct, *args):
     returnME = None
     try:
-        sum = fct(args[0], args[1])
+        sum = fct(*args)
         return sum
     except Exception as e:
-        print("Exception: {}".format(e))
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return returnME
 
 
