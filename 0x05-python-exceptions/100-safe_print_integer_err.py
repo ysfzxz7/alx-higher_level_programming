135f90de2033cf5107f631f9bdb56e3745e9a976
ysfzxz7 <bbbmaroc83@gmail.com>
2023-11-20 16:19:29 +0100

    safe 100 advancedclear

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        return True
+        return (True)
     except (ValueError, TypeError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return False
+        return (False)
 
 
 if (__name__ == "__main__"):
