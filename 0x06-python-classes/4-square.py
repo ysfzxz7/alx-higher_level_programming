0f76dcbc06686ada4e06571a0229b9b18da9e745
ysfzxz7 <bbbmaroc83@gmail.com>
2023-11-21 15:37:43 +0100

    task '

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,14 +7,24 @@ class Square:
     This is an exemple of class ..
     """
     def __init__(self, size=0):
-        if (type(size)) != int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        """ init a new square
+        Agrs =>:
+                size (int): The size must be < 0
+        """
+        self.size = size
+    @property
+    def size(self):
+        """This methode serve a getter to the value size"""
+        return (self.__size)
 
+    @size.setter
+    def size(self, value):
+        """This methode serve as setter to the size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an intger")
+        if value < 0:
+            raise ValueError("size must be >=0")
+        self.__size = value    
+    
     def area(self):
         return self.__size * self.__size
-
-    def size(self):
-        return self.__size
