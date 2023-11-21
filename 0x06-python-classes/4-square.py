da79c7e2fe04c26a8d0ed0f4b6fcf0bb838c10b5
ysfzxz7 <bbbmaroc83@gmail.com>
2023-11-21 15:43:15 +0100

    task '

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -22,10 +22,11 @@ class Square:
     def size(self, value):
         """This methode serve as setter to the size"""
         if not isinstance(value, int):
-            raise TypeError("size must be an intger")
+            raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >=0")
         self.__size = value
 
     def area(self):
+        """This methode return the Area of the square"""
         return self.__size * self.__size
