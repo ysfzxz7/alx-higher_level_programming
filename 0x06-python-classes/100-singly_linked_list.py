827d0ea5be34e07b98b6cc83acebf78198cd73bd
ysfzxz7 <bbbmaroc83@gmail.com>
2023-11-22 00:44:06 +0100

    last

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -12,8 +12,8 @@ class Node:
         data: the data stored
         next_node: ptr to the next node
         """
-        self.__data = data
-        self.__next_node = next_node
+        self.data = data
+        self.next_node = next_node
 
     @property
     def data(self):
