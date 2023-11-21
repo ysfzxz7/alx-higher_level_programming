8dcb0a11ab71dcccb025d09ccfbd1f3a52cc550d
ysfzxz7 <bbbmaroc83@gmail.com>
2023-11-21 15:02:40 +0100

    Task 0

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""respresention of class Square """
+
+
+class Square:
+    """
+    This is an exemple of class ..
+    """
+    def __init__(self, size):
+        self.__size = size
