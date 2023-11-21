38d8b6d1e1f35a2d6a600352cee343e1072b4ad5
ysfzxz7 <bbbmaroc83@gmail.com>
2023-11-21 15:22:38 +0100

    task '

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""respresention of class Square """
+
+
+class Square:
+    """
+    This is an exemple of class ..
+    """
+    def __init__(self, size=0):
+        if (type(size)) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def size(self):
+        return self.__size
