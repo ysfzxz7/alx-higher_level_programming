0924318a16625101d6ff0eb58811f0eb128aa847
ysfzxz7 <bbbmaroc83@gmail.com>
2024-01-16 07:04:46 -0800

    show dbs

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- selct name and score
+-- records are ordered
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,5 @@
+-- select all score  less than or equal to 10
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `score` => 10
+ORDER BY `score` DESC;
