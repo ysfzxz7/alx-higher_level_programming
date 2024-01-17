a248d41509a78c853653509c88afd1d409d8825c
ysfzxz7 <bbbmaroc83@gmail.com>
2024-01-17 11:59:00 -0800

    creat new

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- READ users
+CREATE IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
