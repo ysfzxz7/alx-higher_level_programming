814f15ee9a8be9d9a69377816e2f0e5abca81bc4
ysfzxz7 <bbbmaroc83@gmail.com>
2024-02-12 08:33:58 -0800

    2-file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,5 +3,5 @@
 const numArgs = process.argv.length;
 
 console.log(
-	numArgs === 2 ? "No argument" : numArgs === 3 ? "Argument found" : "Argument found"
+	numArgs === 2 ? 'No argument' : numArgs === 3 ? 'Argument found' : 'Argument found'
 );
