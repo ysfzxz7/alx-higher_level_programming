a7a03e67f68b5d7efea1a399886b0926f428ac03
ysfzxz7 <bbbmaroc83@gmail.com>
2024-02-12 10:11:17 -0800

    7-file

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
 
 const x = Math.floor(Number(process.argv[2]));
-if (x < 0 || isNaN(x)) console.log('Missing number of occurrences');
-for (let i = 0; i < x; i++) console.log('C is fun');
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+for (let i = 0; i < x; i++) {
+  console.log('C is fun');
+  }
+}
