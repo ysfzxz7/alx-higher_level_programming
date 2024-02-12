ed50a4edc67aee2372948e02f06284b8b1e38a99
ysfzxz7 <bbbmaroc83@gmail.com>
2024-02-12 09:25:51 -0800

    5-file

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,4 +2,4 @@
 
 const arg1 = Math.floor(Number(process.argv[2]));
 
-console.log(isNaN(arg1) ? 'My number: ' + arg1 : 'Not a number');
+console.log(!isNaN(arg1) ? 'My number: ' + arg1 : 'Not a number');
