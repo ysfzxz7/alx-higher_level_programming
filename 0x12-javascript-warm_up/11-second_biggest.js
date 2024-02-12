4689cd4f20c28ea0264287f8648767e2726a93f5
ysfzxz7 <bbbmaroc83@gmail.com>
2024-02-12 10:45:39 -0800

    11-file

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const myObject = {
-  type: 'object',
-  value: 12
+
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 };
-console.log(myObject);
-/*
-YOUR CODE HERE
-*/
-console.log(myObject);
