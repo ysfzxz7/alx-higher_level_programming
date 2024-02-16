824658e3e51efa9365d3f19479e277a3c9326c14
ysfzxz7 <bbbmaroc83@gmail.com>
2024-02-16 05:12:54 -0800

    file 3

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-A script that takes in an argument and displays all values
-in the states table of db  where name matches the argument.
-Usage: ./2-my_filter_states.py <mysql username>\
-        <mysql password>\
-        <database name>\
-        <state name searched>
+Displays all values in the states table of the database hbtn_0e_0_usa
+whose name matches that supplied as argument.
+Usage: ./2-my_filter_states.py <mysql username> \
+                                <mysql password> \
+                                <database name> \
+                                <state name searched>
 """
 import sys
 import MySQLdb
