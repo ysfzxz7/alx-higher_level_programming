5f2bd8b70ca88f4f48062f1dae19b696ccab3aac
ysfzxz7 <bbbmaroc83@gmail.com>
2024-02-16 05:12:15 -0800

    file 3

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user = sys.argv[1], passwd = sys.argv[2], db = sys.argv[3])
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM `states`")
+    cursor.execute("SELECT * FROM `states` ORDER BY `id`")
     for state in cursor.fetchall():
         if state[1] == sys.argv[4]:
             print(state)
