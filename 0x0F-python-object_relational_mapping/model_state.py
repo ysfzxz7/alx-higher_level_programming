9e2dc028033835eb04c44dfa8ead35f985127c91
ysfzxz7 <bbbmaroc83@gmail.com>
2024-02-16 06:58:18 -0800

    file class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -16,6 +16,6 @@ class State(Base):
     id (sqlalchemy.Integer): The state's id.
     name (sqlalchemy.String): The state's name.
     """
-    __tablename__ = 'states'
-    id = Column(Integer, primary_Key=True)
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
     name = Colmun(String(128), nullable=False)
