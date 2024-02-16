fe66040729a8d29da656c4c3806d66fc5f079eab
ysfzxz7 <bbbmaroc83@gmail.com>
2024-02-16 04:39:23 -0800

    file 2

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user = sys.argv[1], passwd = sys.argv[2], db = sys.argv[3])
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM `states`")
+    for state in cursor.fetchall():
+        if state[1][0] == "N":
+            print(state)
