7087dcdf9d62784d107bb46c2f6cdcbef70407d8
ysfzxz7 <bbbmaroc83@gmail.com>
2024-03-01 13:37:50 -0800

    id

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,12 +7,12 @@ Usage:
     ./7-error_code.py URL.
 """
 
-import sys
-import requests
-if __name__ == "__main__":
-    url = sys.argv[1]
-    res = requests.get(url)
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    res = requests.get(sys.argv[1])
     if res.status_code >= 400:
-        print({'Error code: {}'}.format(res.status_code))
+        print('Error code: {}'.format(res.status_code))
     else:
         print(res.text)
