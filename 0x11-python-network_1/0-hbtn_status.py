371309fa6a624082ebfb867f8309368ded474ea8
ysfzxz7 <bbbmaroc83@gmail.com>
2024-03-01 11:25:42 -0800

    file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,9 @@ import urllib.request
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
-    response = urllib.request.urlopen(url)
-    data = response.read()
-    print(f"""Body response:
-    - type: {type(data)}
-    - content: {data}
-    - utf8 content: {data.decode('utf-8')}""")
+    with urllib.request.urlopen(url) as respose:
+        data = response.read()
+        print(f"""Body response:
+        - type: {type(data)}
+        - content: {data}
+        - utf8 content: {data.decode('utf-8')}""")
