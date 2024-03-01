40fa556e0112801ea68d0706d736455b63bf5f00
ysfzxz7 <bbbmaroc83@gmail.com>
2024-03-01 13:25:24 -0800

    id

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,9 +5,8 @@ This how to handle errors in reqs lib
 import sys
 import requests
 if __name__ == "__main__":
-    try:
-        url = sys.argv[1]
-        res = requests.get(url)
-        print(res.text)
-    except requests.exceptions.HTTPErrors as e:
-        print({'Error code: {}'}.format(e.response.status_code))
+    url = sys.argv[1]
+    res = requests.get(url)
+    print(res.text)
+    if res.status_code >= 400
+    print({'Error code: {}'}.format(res.status_code))
