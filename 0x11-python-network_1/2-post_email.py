e7213644d8563f18b64815e8eb609bd723ef4e76
ysfzxz7 <bbbmaroc83@gmail.com>
2024-03-01 12:10:11 -0800

    file1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,7 +8,7 @@ import urllib.parse
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    encoded_data = urllib.parse.urlencode({'email' : email}).encode('ascii')
+    encoded_data = urllib.parse.urlencode({'email': email}).encode('ascii')
     req = urllib.request.Request(url, data=encoded_data)
     with urllib.request.urlopen(req) as res:
         html = res.read()
