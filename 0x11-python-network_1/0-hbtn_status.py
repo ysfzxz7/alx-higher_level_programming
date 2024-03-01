cfed45ff00c7a3c5ed25a13eedf6e841d79eeedf
ysfzxz7 <bbbmaroc83@gmail.com>
2024-03-01 11:23:09 -0800

    file

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    print(f"""Body response:
+    - type: {type(data)}
+    - content: {data}
+    - utf8 content: {data.decode('utf-8')}""")
