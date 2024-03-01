7751371562282c229cd9658fe06142fa13735c6f
ysfzxz7 <bbbmaroc83@gmail.com>
2024-03-01 12:09:01 -0800

    file1

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This script is an exemple of post req by urllib
+"""
+import sys
+import urllib.request
+import urllib.parse
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    encoded_data = urllib.parse.urlencode({'email' : email}).encode('ascii')
+    req = urllib.request.Request(url, data=encoded_data)
+    with urllib.request.urlopen(req) as res:
+        html = res.read()
+    print(html.decode('utf-8'))
